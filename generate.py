d4a6fbba8a61a18f85babac942dc5c6863c19837
Vincent Garcia <vincentfpgarcia@gmail.com>
2023-07-10 22:01:58 +0200

    feat: adding intro video

---
+++ generate.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from shutil import copyfile
+import fileinput
+
+
+content = [
+  ['Project description', 'index.txt'],
+  ['Intro video', 'intro.txt'],
+  ['Demo video', 'demo.txt']
+]
+
+for title, input_path in content:
+
+  print('Processing %s' % input_path)
+
+  # Get the text of the considered file
+  text = ''
+  with open(input_path, 'r') as f:
+    text = f.read()
+
+  # Open the HTML template
+  with open('template.txt', 'r') as input_file:
+
+    # Open the output file
+    output_path = os.path.splitext(input_path)[0] + '.html'
+    with open(output_path, 'w') as output_file:
+
+      # Get the current line from the template
+      for line in input_file:
+
+        # Eventually modify it (title or content)
+        if '$title' in line:
+          line = line.replace('$title', title)
+        elif '$content' in line:
+          line = line.replace('$content', text)
+
+        # Write the line into the output file
+        output_file.write(line)
